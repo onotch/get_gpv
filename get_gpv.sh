4ea733e2f4cbf9fd67a3349ba5c84897aeb538b3
onotch <onotch.x@gmail.com>
2021-01-09 15:29:31 +0900

    improved timing to acquire images

--- get_gpv.sh
+++ get_gpv.sh
@@ -33,15 +33,16 @@ if [ ! -d ${LOG_DIR} ]; then
   mkdir -p ${LOG_DIR}
 fi
 
-year=`date -d '3 hours ago' +'%Y'`
-month=`date -d '3 hours ago' +'%m'`
-day=`date -d '3 hours ago' +'%d'`
-hour=`date -d '3 hours ago' +'%H'`
+hour_delta=$((`date +'%k'`%3+3))
+year=`date -d "$((hour_delta)) hours ago" +"%Y"`
+month=`date -d "$((hour_delta)) hours ago" +"%m"`
+day=`date -d "$((hour_delta)) hours ago" +"%d"`
+hour=`date -d "$((hour_delta)) hours ago" +"%k"`
 log_file_path="${LOG_DIR}/error_log_`date +'%Y%m%d'`.txt"
 
 for type in ${TYPE[@]}; do
   for area in ${AREA[@]}; do
-    url_html=${GPV_URL}/msm_${type}_${area}_${year}${month}${day}${hour}.html
+    url_html=${GPV_URL}/msm_${type}_${area}_${year}${month}${day}`printf %02d ${hour}`.html
     response=`curl -s -o ${TMP_HTML_FILE} -w "%{http_code}" ${url_html}`
 
     if [ ${response} = "200" ]; then
@@ -50,9 +51,9 @@ for type in ${TYPE[@]}; do
         mkdir -p ${save_dir}
       fi
 
-      for ((i=1; i<=3; i++)); do
-        filename=`grep "fnl\[${i}\]" ${TMP_HTML_FILE} | awk -F'["]' '{print $2}'`
-        save_hour=`date -d "$((4-i)) hours ago" +"%H"`
+      for ((i=0; i<3; i++)); do
+        filename=`grep "fnl\[$((i+1))\]" ${TMP_HTML_FILE} | awk -F'["]' '{print $2}'`
+        save_hour=`printf %02d $((hour+i))`
         save_file_path="${save_dir}/msm_${type}_${area}_${year}${month}${day}${save_hour}.png"
 
         curl -s -o ${save_file_path} ${GPV_URL}/msm/${filename}
